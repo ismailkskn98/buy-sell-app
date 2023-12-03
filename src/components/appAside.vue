857a58cdedfc442e37f9a0151f286a2e4974fff8
İsmail Keskin <hamdismailkskn98@gmail.com>
2023-12-03 06:15:27 +0300

    main layout grid yapısı oluşturuldu

---
+++ src/components/appAside.vue
@@ -0,0 +1,15 @@
+<template>
+    <aside>
+        <ul>
+            <li>ürünlerim</li>
+            <li>ürün oluştur</li>
+            <li>favorilerim</li>
+            <li>kayıt edilenler</li>
+            <li>kategoriler</li>
+        </ul>
+    </aside>
+</template>
+
+<script setup>
+
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,15 @@
 <template>
-    <div class="container">
-        <slot />
+    <div class="main-layout">
+        <appHeader />
+        <appAside />
+        <main>
+            <h2>Anasayfa</h2>
+            <slot />
+        </main>
     </div>
 </template>
 
 <script setup>
-
+import appHeader from '../components/appHeader.vue';
+import appAside from '../components/appAside.vue';
 </script>
-
-<style lang="scss" scoped></style>
